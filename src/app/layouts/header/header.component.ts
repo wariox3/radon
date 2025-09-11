3671424776c4715d0813df5abcb7c67665df7958
sebastian <sebastian.h.piedrahita@gmail.com>
2025-09-11 08:59:34 -0500

    style: agregar padding x en header

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -13,7 +13,7 @@ import { RouterLink } from '@angular/router';
 })
 export class HeaderComponent {
   @HostBinding('class') hostClass =
-    'fixed top-0 z-10 left-0 right-0 flex items-stretch shrink-0 bg-[#fefefe] dark:bg-coal-500 shadow-sm dark:border-b dark:border-b-coal-100';
+    'fixed top-0 px-7 z-10 left-0 right-0 flex items-stretch shrink-0 bg-[#fefefe] dark:bg-coal-500 shadow-sm dark:border-b dark:border-b-coal-100';
   @HostBinding('attr.role') hostRole = 'banner';
   @HostBinding('attr.data-sticky') dataSticky = 'true';
   @HostBinding('attr.data-sticky-name') dataStickyName = 'header';
